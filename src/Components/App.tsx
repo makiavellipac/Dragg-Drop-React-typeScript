1ba636a22b13e643659fdcfd67260a572851b05b
Santiago <santiago@LAPTOP-93ND2TI6.localdomain>
2021-07-29 12:41:49 -0500

    Se modifico el componente App para pintar la data

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,9 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import initialData from '../Data/initial-data';
+import { initialDataType } from '../Types/taks';
+import Column from './Column/Column';
 
-const App: FC = () => (
-  <div className="div-app">
-    <h1> Que Pedal!</h1>
-  </div>
-);
+const App: FC = () => {
+  const [state, setState] = useState<initialDataType>(initialData);
+  return (
+    <>
+      {state.columnOrder.map((columnId) => {
+        const column = state.columns[columnId];
+        const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
+        return <Column key={column.id} column={column} tasks={tasks} />;
+      })}
+    </>
+  );
+};
 
 export default App;
