a4fceb561b5db69cd7c113214df7fa9f2eb01aac
Santiago <santiago@LAPTOP-93ND2TI6.localdomain>
2021-07-30 17:48:29 -0500

    Se agrego la logica para agregar columnas y tareas

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -146,7 +146,30 @@ const App: FC = () => {
         <Button
           variant="outlined"
           color="primary"
-          style={{ width: '150px', height: '56px' }}>
+          style={{ width: '150px', height: '56px' }}
+          onClick={() => {
+            const nameColumn = `column-${state.columnOrder.length + 1}`;
+            const column: {
+              [key: string]: {
+                id: string;
+                title: string;
+                taskIds: string[];
+              };
+            } = {
+              [nameColumn]: {
+                id: nameColumn,
+                title: inputs.columns,
+                taskIds: [],
+              },
+            };
+            const newInitialData: initialDataType = {
+              ...state,
+              columns: { ...state.columns, ...column },
+              columnOrder: [...state.columnOrder, nameColumn],
+            };
+            setState(newInitialData);
+            setInputs({ columns: '', task: '' });
+          }}>
           Añadir Columna
         </Button>
       </Container>
@@ -169,7 +192,33 @@ const App: FC = () => {
         <Button
           variant="outlined"
           color="primary"
-          style={{ width: '150px', height: '56px' }}>
+          style={{ width: '150px', height: '56px' }}
+          onClick={() => {
+            const nameTask = `task-${Object.keys(state.tasks).length + 1}`;
+            const auxTasks: {
+              [key: string]: {
+                id: string;
+                content: string;
+              };
+            } = {
+              [nameTask]: {
+                id: nameTask,
+                content: inputs.task,
+              },
+            };
+            const tas = state.columns['column-1'].taskIds;
+            tas.push(nameTask);
+            const newInitialData: initialDataType = {
+              ...state,
+              columns: {
+                ...state.columns,
+                'column-1': { ...state.columns['column-1'], taskIds: tas },
+              },
+              tasks: { ...state.tasks, ...auxTasks },
+            };
+            setState(newInitialData);
+            setInputs({ columns: '', task: '' });
+          }}>
           Añadir Task
         </Button>
       </Container>
